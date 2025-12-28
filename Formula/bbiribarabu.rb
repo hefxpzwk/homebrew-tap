54ddf7aa8383c533bba4fb5c4e505077ac984262
hefxpzwk <hefxpzwk@gmail.com>
2025-12-29 02:27:35 +0900

    Update bbiribarabu to v0.1.7

--- Formula/bbiribarabu.rb
+++ Formula/bbiribarabu.rb
@@ -2,7 +2,7 @@ class Bbiribarabu < Formula
   desc "Git context-aware terminal with branch-based voice logs"
   homepage "https://github.com/hefxpzwk/Bbiribarabu"
   url "https://github.com/hefxpzwk/Bbiribarabu/releases/download/v0.1.7/bbiribarabu-macos"
-  sha256 "sha256:0aac11bd68ad9626b47d57eb3b7e1f24573156377fdcc1bf39220206320ff361"
+  sha256 "0aac11bd68ad9626b47d57eb3b7e1f24573156377fdcc1bf39220206320ff361"
 
   def install
     bin.install "bbiribarabu-macos" => "bbiribarabu"
